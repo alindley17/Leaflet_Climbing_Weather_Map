1c87806cf9ed50cada90e01566a6e98d0c52f9ad
alindley17 <Mac@Andrew-Lindleys-MacBook-Pro.local>
2017-04-26 13:39:35 -0600

    4/26 changes
    working mapbox baseman and static aeris layer
    
    css header not working

---
+++ js/leaflet-code.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    var mymap = L.map('mapdiv').setView([39, -105.5], 7);
+
+    L.tileLayer('https://api.mapbox.com/styles/v1/alindley/cj0smo4ll00c62smpbr2dpehd/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYWxpbmRsZXkiLCJhIjoiY2l5Y2JlcmViMDBvczMyc2N1eTA4MDd4MSJ9.t0Z60l8r6_8DNbX7xB1bnA', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.satellite',
+        accessToken: 'pk.eyJ1IjoiYWxpbmRsZXkiLCJhIjoiY2l5Y2JlcmViMDBvczMyc2N1eTA4MDd4MSJ9.t0Z60l8r6_8DNbX7xB1bnA'
+    }).addTo(mymap);
+
+    L.tileLayer('https://maps.aerisapi.com/CSJEnhBz47Lj5Rnz1qOMJ_syBX7hid79IHKhZszE0qC6rmyNx8YNTSmb9NprmT/radar/{z}/{x}/{y}/current.png', {
+        subdomains: '1234',
+        attribution: '&copy;AerisWeather',
+    }).addTo(mymap);
+
+});
